906ee3863a73783c168b5f954551f2ffcd9f1cc5
homework <397006371@qq.com>
2016-05-31 21:52:37 +0800

    hw

--- wechat/views.py
+++ wechat/views.py
@@ -60,8 +60,8 @@ def WeChat(request):
                 if wechat.message.type == 'click':
                     return HttpResponse(wechat.response_text('这是测试回复！！', escape=False))
             elif isinstance(wechat.message, TextMessage):
-                # if wechat.message.content =='交流':
-                    return HttpResponse(wechat.response_text('test_jiaoliu', escape=False))
+                 if wechat.message.content =='交流':
+                    return HttpResponse(wechat.response_text('<a href ="http://yunzhijia.com/36FkG">点我进入社区</a>', escape=False))
     except WechatAPIException, e:
              return  HttpResponse('errcode:'+str(e.errcode)+'<br/>errmsg:'+e.errmsg)
 
