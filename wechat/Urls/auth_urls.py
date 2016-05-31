513575a38088ad26313e798ad2b03aeee572ad8d
wjy <397006371@qq.com>
2016-05-31 09:11:43 +0800

    cm

--- wechat/Urls/auth_urls.py
+++ wechat/Urls/auth_urls.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'wangjingyao'
 from django.conf.urls import patterns, include, url
-from wechat.views import WeChat
+from wechat.views import *
 
 auth_partterns = patterns('',
   url(r'^$', WeChat),
+  url(r'^init_conf/', init_conf),
 )
