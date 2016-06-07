4fd0410d0f60270dbbb30915c62a9741269bb2cd
wjy <397006371@qq.com>
2016-06-07 10:36:39 +0800

    wc

--- wechat_huoyun/urls.py
+++ wechat_huoyun/urls.py
@@ -17,9 +17,36 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from wechat.Urls.auth_urls import auth_partterns
 from wechat.views import *
+from wechat_huoyun import settings
+import os
 
 urlpatterns = [
+    url(r'^static/wechat/js/(?P<path>.*)$', 'django.views.static.serve'),
+    url(r'^static/wechat/css/(?P<path>.*)$', 'django.views.static.serve'),
+    url(r'^static/wechat/images/(?P<path>.*)$', 'django.views.static.serve'),
+    url(r'^static/wechat/upload/(?P<path>.*)$', 'django.views.static.serve'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^wechat/', include(auth_partterns)),
     url(r'^login.html/', index),
 ]
+    # if settings.DEBUG:
+    #     url( r'^css/(?P<path>.*)$', 'django.views.static.serve',
+    #             { 'document_root': ROOT+'/css' }
+    #     ),
+    #     url( r'^js/(?P<path>.*)$', 'django.views.static.serve',
+    #             { 'document_root': ROOT+'/js' }
+    #     ),
+    #     url( r'^images/(?P<path>.*)$', 'django.views.static.serve',
+    #             { 'document_root': ROOT+'/images' }
+    #     ),
+    #     url( r'^ueEditor/(?P<path>.*)$', 'django.views.static.serve',
+    #             { 'document_root': ROOT+'/ueEditor' }
+    #     ),
+# if settings.DEBUG:
+#     urlpatterns += [
+#     url(r'^static/wechat/js/(?P<path>.*)$', 'django.views.static.serve'),
+#     url(r'^static/wechat/css/(?P<path>.*)$', 'django.views.static.serve'),
+#     url(r'^static/wechat/images/(?P<path>.*)$', 'django.views.static.serve)',
+#     url(r'^static/wechat/upload/(?P<path>.*)$', 'django.views.static.serve)',
+    # ({'document_root': os.path.join(settings.SITE_ROOT,'media')},name="media"),
+    # ]
